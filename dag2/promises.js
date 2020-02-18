28f8e483ab2d589f3684d0d41cada1159f32111e
Jeroen Nederhoed <jeroen@nederhoed.net>
2020-02-18 14:08:58 +0100

    promise opdrachten gedaan

---
+++ dag2/promises.js
@@ -0,0 +1,55 @@
+const testNum = number => {
+	return new Promise((resolve, reject) => {
+		if (number <= 10) {
+			resolve('kleiner dan 10');
+		} else {
+			reject('groter dan 10');
+		}
+	});
+};
+
+console.log(testNum(5));
+
+const makeAllCaps = array => {
+	return new Promise((resolve, reject) => {
+		if (
+			array.every(word => {
+				return typeof word === 'string';
+			})
+		) {
+			resolve(
+				sortWords(
+					array.map(word => {
+						return word.toUpperCase();
+					})
+				)
+			);
+		} else {
+			reject('Not a string!');
+		}
+	});
+};
+
+const sortWords = array => {
+	return new Promise((resolve, reject) => {
+		if (array) {
+			resolve(array.sort());
+		} else {
+			reject('strings only!');
+		}
+	});
+};
+
+const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
+const complicatedArray = ['cucumber', 44, true];
+//console.log(makeAllCaps(arrayOfWords));
+//console.log(sortWords(arrayOfWords));
+makeAllCaps(arrayOfWords)
+	.then(sortWords(arrayOfWords))
+	.then(result => console.log(result))
+	.catch(error => console.log(error));
+
+makeAllCaps(complicatedArray)
+	.then(sortWords(complicatedArray))
+	.then(result => console.log(result))
+	.catch(error => console.log(error));
