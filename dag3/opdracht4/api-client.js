ff6fdfc3ebce26587c22e86e89ed216cdb937f8e
Jeroen Nederhoed <jeroen@nederhoed.net>
2020-02-20 14:17:25 +0100

    opdracht film-lijsten eindelijk afgerond

--- dag3/opdracht4/api-client.js
+++ dag3/opdracht4/api-client.js
@@ -1,17 +1,2 @@
 const API_KEY = '3a64966d65a4e0a4bbab54f559afdaf9';
-
-// getGenres = async () => {
-// 	try {
-// 		const apiUrl =
-// 			'https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY;
-// 		const res = await fetch(apiUrl, {
-// 			method: 'GET'
-// 		});
-// 		console.log('json fetched');
-// 		const data = await res.json();
-// 		console.log(JSON.stringify(data.genres));
-// 		return data;
-// 	} catch {
-// 		console.log('Oops an error: ', err);
-// 	}
-// };
+// https://api.themoviedb.org/3/movie/top_rated?api_key=3a64966d65a4e0a4bbab54f559afdaf9
--- dag3/opdracht4/script.js
+++ dag3/opdracht4/script.js
@@ -10,20 +10,39 @@ getGenres = async () => {
 		const res = await fetch(apiUrl, {
 			method: 'GET'
 		});
-		console.log('json fetched');
+		//console.log('json fetched');
 		const data = await res.json();
-		console.log(JSON.stringify(data.genres));
+		//console.log(JSON.stringify(data.genres));
 		return data;
 	} catch {
 		console.log('Oops an error: ', err);
 	}
 };
 
+//haal lijst object op
+const getObject = async (url, list) => {
+	console.log(url);
+	try {
+		const res = await fetch(url, {
+			method: 'GET'
+			//headers: ''
+		});
+		//console.log('json fetched');
+		const data = await res.json();
+		dataObject = JSON.stringify(data);
+		//console.log(dataObject);
+		formatData(dataObject, list);
+	} catch {
+		console.log('Oops an error: ');
+	}
+};
+//
+
 const callApi = async () => {
 	movieGenres = await getGenres();
 	const genres = movieGenres.genres;
 	const listIntoDom = await genres.forEach(genre => {
-		console.log(genre);
+		//console.log(genre);
 		let ul = document.getElementById('genre-list');
 
 		ul.appendChild(
@@ -32,4 +51,33 @@ const callApi = async () => {
 	});
 };
 
+//maak hapklare data van Object
+const formatData = async (moviedata, list) => {
+	//console.log('regel56: ', url);
+	//retrievedData = await getObject(url);
+	movieArray = JSON.parse(moviedata).results;
+	console.log(movieArray);
+	dataToDom(movieArray, list);
+};
+
+//plaats lijst in DOM
+const dataToDom = async (movies, list) => {
+	//console.log(movies);
+	await movies.forEach(movie => {
+		console.log(`genre naam:  ${movie.title}, id: ${movie.id}`);
+		let ul = document.getElementById(list);
+
+		ul.appendChild(
+			document.createElement('li')
+		).innerHTML = `${movie.vote_average} - ${movie.title}, id: ${movie.id}`;
+	});
+};
+
 callApi();
+
+const topRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=1`;
+getObject(topRated, 'top-rated-movies');
+const topRatedAction = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&with_genres=28`;
+getObject(topRatedAction, 'top-rated-action-movies');
+const topRatedOldies = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1975`;
+getObject(topRatedOldies, 'top-rated-oldies');
